9350450a1f1c7089b63886eeaf5ba471db39087e
David J. Cox <cox.david.j@gmail.com>
2020-06-11 19:56:20 -0400

    Update EDA.py

--- Police Data/EDA.py
+++ Police Data/EDA.py
@@ -24,17 +24,26 @@ os.chdir('C:/Users/David-PC/Dropbox/Projects/CurrentProjectManuscripts/Empirical
 # Change settings to view all columns of data
 pd.set_option('display.max_columns', None)
 
-#%%
-
-import urllib.request
-link = 'https://stacks.stanford.edu/file/druid:yg821jf8611/yg821jf8611_tn_nashville_2020_04_01.csv.zip'
-urllib.request.urlretrieve(link, "yg821jf8611_tn_nashville_2020_04_01.csv.zip")
-compressed_file = zipfile.ZipFile('yg821jf8611_tn_nashville_2020_04_01.csv')
-                                  
-
-
 #%% Read in the data
-raw_data = pd.read_csv(r)
+raw_data = pd.read_csv('tn_nashville_2020_04_01.csv', low_memory=False)
 data = raw_data.copy()
-data.head()
+list(data)
+len(data)
 
+#%% Plot the number of times 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+race_counts = data.subject_race.value_counts()
+plt.figure(figsize=(10,5))
+sns.barplot(race_counts.index, race_counts.values, color='black')
+plt.title('Nashville, TN (2010-2019)')
+plt.ylabel('Number of Stops', fontsize=16)
+plt.xlabel('Race', fontsize=16)
+plt.show()
+
+#%% Create list of office ids for separating data out
+officers = data.officer_id_hash.unique()
+
+data.date.max()
+data.date.min()

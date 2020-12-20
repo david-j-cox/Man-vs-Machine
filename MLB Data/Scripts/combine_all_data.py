2184002d134e746cd1568bca8f135109f9f23d8c
David J. Cox <cox.david.j@gmail.com>
2020-12-20 16:52:58 -0500

    reg commit

---
+++ MLB Data/Scripts/combine_all_data.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: 
+    David J. Cox, PhD, MSB, BCBA-D
+    cox.david.j@gmail.com
+    https://www.researchgate.net/profile/David_Cox26
+    twitter: @davidjcox_
+    LinkedIn: https://www.linkedin.com/in/coxdavidj/
+    Website: https://davidjcox.xyz
+"""
+#Set current working directory to the folder that contains your data.
+import os
+import pandas as pd
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+# Set path to local scripts
+sys.path.append('/Users/davidjcox/Dropbox/Coding/Local Python Modules/')
+
+# Set path to data
+direcotry = '/Users/davidjcox/Dropbox (Personal)/Projects/CurrentProjectManuscripts/Empirical/PersonalFun/Matching/KaggleWebscrapingAnalysis/Man-vs-Machine/MLB Data/Team Data/'
+os.chdir(directory)
+
+# Change settings to view all columns of data
+pd.set_option('display.max_columns', None)
+
+#%% Read in the data
+raw_data = pd.read_csv()
+data = raw_data.copy()
+data.head()
+
+#%%

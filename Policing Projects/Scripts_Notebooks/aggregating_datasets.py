2b42b4654a56fde12089be8cdaf8d923e1f42230
David J. Cox <cox.david.j@gmail.com>
2020-12-25 07:52:38 -0500

    reg commit

--- Policing Projects/Scripts_Notebooks/aggregating_datasets.py
+++ Policing Projects/Scripts_Notebooks/aggregating_datasets.py
@@ -53,12 +53,53 @@ for subdir, dirs, files in os.walk(directory):
             err_type.append(err)
             troublesome.append(filename)
 
-#%% Combine into single dataframe and save
-all_df = pd.concat(all_data)
-all_df.to_csv('all_data.csv')
-
-#%% Add year and month to df as unique cols
+# Add year and month to df as unique col
 all_df['year'] = pd.DatetimeIndex(all_df['date']).year
 all_df['month'] = pd.DatetimeIndex(all_df['date']).month
 
-#%%
+# Combine into single dataframe and save
+all_df = pd.concat(all_data)
+all_df.to_csv('all_data.csv')
+
+#%% Some one-off stats for the ms
+all_df.year.describe()
+len(all_df.officer_id_hash.unique())
+
+#%% Create individual data frames for the different independent and dependent variables
+offcr_stops = data.groupby(['year', 'month', 'officer_id_hash']).size()
+offcr_cites = data.groupby(['year', 'month', 'officer_id_hash', 'citation_issued']).size()
+offcr_search = data.groupby(['year', 'month', 'officer_id_hash', 'search_conducted']).size()
+offcr_frisk = data.groupby(['year', 'month', 'officer_id_hash', 'frisk_performed']).size()
+offcr_contra = data.groupby(['year', 'month', 'officer_id_hash', 'contraband_found']).size()
+offcr_arrest = data.groupby(['year', 'month', 'officer_id_hash', 'arrest_made']).size()
+
+#%% Print descriptions of each subset dataframe 
+from scipy.stats import skew
+grouped_data = [offcr_stops, offcr_cites, offcr_search, offcr_frisk, offcr_contra, offcr_arrest]
+data_type = ['Stops', 'Citations', 'Searches', 'Frisks', 'Contraband Found', 'Arrests']
+
+for i in range(len(grouped_data)):
+  print(data_type[i], "| Skew =", round(skew(grouped_data[i]), 4), "\n", grouped_data[i].describe(), "\n")
+  
+#%% Plot letter-value plots / boxen plots of the different distributions. 
+def boxen(df, x_label):
+  f, ax = plt.subplots(figsize=(2, 5))
+  sns.boxenplot(y=df, color='white')
+  plt.xlabel(x_label, fontsize=20, labelpad=(16))
+  plt.ylabel('Count per Month', fontsize=20, labelpad=(16))
+  plt.ylim(.1, 1000)
+  plt.yscale("log")
+  plt.yticks(ticks=[.1, 1, 10, 100], labels=['0.1', '1', '10', '100'], fontsize=12)
+  # plt.yticks(fontsize=12)
+  right_side = ax.spines["right"]
+  right_side.set_visible(False)
+  top = ax.spines["top"]
+  top.set_visible(False)
+  plt.show()
+
+boxen(offcr_stops, 'Stops')
+boxen(offcr_cites, 'Citations')
+boxen(offcr_search, 'Searches')
+boxen(offcr_frisk, 'Frisks')
+boxen(offcr_contra, 'Contraband')
+boxen(offcr_arrest, 'Arrests')

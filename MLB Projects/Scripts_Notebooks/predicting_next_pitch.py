77a952bec168d9b242b04dc65eec15c776caee9b
David J. Cox <cox.david.j@gmail.com>
2021-01-24 17:21:38 -0500

    reg commit

--- MLB Projects/Scripts_Notebooks/predicting_next_pitch.py
+++ MLB Projects/Scripts_Notebooks/predicting_next_pitch.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-@author: 
-    David J. Cox, PhD, MSB, BCBA-D
-    cox.david.j@gmail.com
-    https://www.researchgate.net/profile/David_Cox26
-    twitter: @davidjcox_
-    LinkedIn: https://www.linkedin.com/in/coxdavidj/
-    Website: https://davidjcox.xyz
 """
 #Set current working directory to the folder that contains your data.
 import os
